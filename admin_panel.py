94b8c5836556577062a8b759068fb20294b12bb8
Huseyin Eksici <huseyineksici01@gmail.com>
2024-02-23 14:00:56 +0300

    creating table of database

--- admin_panel.py
+++ admin_panel.py
@@ -4,13 +4,13 @@ db = mysql.connector.connect(
     host = "localhost",
     user = "root",
     passwd = "huseyinilkerh1905",
-    
+    database ="HotelManagementDatabase"
 )
 
 mycursor = db.cursor()
 
-# mycursor.execute("CREATE TABLE People (person_id int PRIMARY KEY AUTO_INCREMENT,Name VARCHAR(50) NOT NULL,Username VARCHAR(50) NOT NULL,E-mail VARCHAR(100) NOT NULL)")
-mycursor.execute("CREATE DATABASE HotelManagementDatabase")
+mycursor.execute("CREATE TABLE People (person_id int PRIMARY KEY AUTO_INCREMENT,Name VARCHAR(50) NOT NULL,Username VARCHAR(50) NOT NULL,Email VARCHAR(100) NOT NULL)")
+
 class DATABASE:
     def __init__(self):
         pass
